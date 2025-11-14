da4965e9ebcef21c952f83969218003450ea4fe4
Pawan kumar <146861571+pawangiri3@users.noreply.github.com>
2025-11-14 22:54:20 +0530

    Feature/rg101 (#6)
    * for rg parent module added
    
    * child added
    
    * pipline folder added
    
    * yaml
    
    * chield module added for some resource
    
    * updated fiels
    
    ---------
    
    Co-authored-by: Pawan

--- terraform-infra/modules/azurerm_resource_group/main.tf
+++ terraform-infra/modules/azurerm_resource_group/main.tf
@@ -1,7 +1,7 @@
 # main.tf for module: azurerm_resource_group
 resource "azurerm_resource_group" "rgs" {
 
-  for_each = var.rgs
+  for_each = var.vms
   name     = each.value.resource_group_name
   location = each.value.location
   tags     = each.value.tags
